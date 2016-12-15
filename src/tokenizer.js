0674e4c509d3a7b8e459e7c4dec29c1e6c09a13b
pnowak <piotr.nowak.25.02.1978@gmail.com>
2016-12-15 22:48:40 +0100

    tokenizer

---
+++ src/tokenizer.js
@@ -0,0 +1,75 @@
+export function tokenizer(code) {
+    let input = code
+                    .replace(/\./gi, ' DOT ')
+                    .replace(/\,/gi, ' COMMA '); console.log(input);
+
+    let current = 0;
+    const tokens = [];
+
+    while (current < input.length) {
+
+        let char = input[current];
+        const WHITESPACE = /\s/;
+        const LETTERS = /[a-z]/i;
+        const NUMBERS = /[0-9]/;
+        
+        if (char === '(') {
+            tokens.push({
+                type: 'paren',
+                value: '('
+            });
+            current++;
+            continue;
+        }
+
+        if (char === ')') {
+            tokens.push({
+                type: 'paren',
+                value: ')'
+            });
+            current++;
+            continue;
+        }
+
+        if (WHITESPACE.test(char)) {
+            current++;
+            continue;
+        }
+
+        if (NUMBERS.test(char)) {
+            let value = '';
+
+            while (NUMBERS.test(char)) {
+                value += char;
+                char = input[++current];
+            }
+
+            tokens.push({
+                type: 'number',
+                value: value
+            });
+
+            continue;
+        }
+
+        if (LETTERS.test(char)) {
+            let value = '';
+
+            while (LETTERS.test(char)) {
+                value += char;
+                char = input[++current];
+            }
+
+            tokens.push({
+                type: 'name',
+                value: value
+            });
+
+            continue;
+        }
+
+        throw new TypeError('I dont know what this character is: ' + char);
+    }
+
+    return tokens;
+}
